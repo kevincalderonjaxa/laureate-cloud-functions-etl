0159191f522b2a9935d5ebf8262fa9df6648f463
kevincalderonjaxa <kevin.calderon@jaxa-consulting.com>
2025-09-08 18:27:19 -0500

    Create default-main.py

---
+++ ETL_PART1/default-main.py
@@ -0,0 +1,26 @@
+import functions_framework
+
+# CloudEvent function to be triggered by an Eventarc Cloud Audit Logging trigger
+# Note: this is NOT designed for second-party (Cloud Audit Logs -> Pub/Sub) triggers!
+@functions_framework.cloud_event
+def hello_auditlog(cloudevent):
+    # Print out the CloudEvent's (required) `type` property
+    # See https://github.com/cloudevents/spec/blob/v1.0.1/spec.md#type
+    print(f"Event type: {cloudevent['type']}")
+
+    # Print out the CloudEvent's (optional) `subject` property
+    # See https://github.com/cloudevents/spec/blob/v1.0.1/spec.md#subject
+    if 'subject' in cloudevent:
+        # CloudEvent objects don't support `get` operations.
+        # Use the `in` operator to verify `subject` is present.
+        print(f"Subject: {cloudevent['subject']}")
+
+    # Print out details from the `protoPayload`
+    # This field encapsulates a Cloud Audit Logging entry
+    # See https://cloud.google.com/logging/docs/audit#audit_log_entry_structure
+
+    payload = cloudevent.data.get("protoPayload")
+    if payload:
+        print(f"API method: {payload.get('methodName')}")
+        print(f"Resource name: {payload.get('resourceName')}")
+        print(f"Principal: {payload.get('authenticationInfo', dict()).get('principalEmail')}")
